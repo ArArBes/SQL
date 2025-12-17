72ce2fdfed70d69b756a0b1acea4fe35c43170b1
ArBes <arina.arifovnaa@gmail.com>
2025-12-17 14:06:39 +0300

    project1

---
+++ sets_joins/ex05/day01_ex05.sql
@@ -0,0 +1,14 @@
+SELECT * 
+FROM person, pizzeria
+ORDER BY person.id, pizzeria.id
+
+-- SELECT 
+--     person.id AS person_id,
+--     person.name AS person_name,
+--     age, gender, address,
+--     pizzeria.id AS pizzeria_id,
+--     pizzeria.name AS pizzeria_name,
+--     rating
+-- FROM person, pizzeria
+-- ORDER BY person.id, pizzeria.id;
+
